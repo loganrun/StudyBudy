fc824cc60f1e10d5f5b39160be778c23b9a9251b
Robert Logan <>
2024-03-21 16:34:24 -0700

    auth routes added

---
+++ backend/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+
+module.exports = function(req, res, next){
+
+    const token = req.header('x-auth-token')
+
+    if(!token){
+        return res.status(401).json({msg: "Improper Credentials"})
+    }
+
+    try{
+
+        const decoded = jwt.verify(token, process.env.jwtSecret)
+        req.user = decoded.user
+        next()
+
+    }catch(err){
+        res.status(401).json({msg: "Invalid Credentials"})
+    }
+
+}
--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {check, validationResult} = reuire('express-validator')
+const User = require("../../models/User")
 
 // @route:   GET api/auth
 // @desc:    Test route
