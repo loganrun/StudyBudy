8d505fbc0f092e07ea44007cd584fb96ddba1dbf
Robert Logan <>
2024-03-21 14:59:23 -0700

    api not working

--- src/components/LectureRecorder.jsx
+++ src/components/LectureRecorder.jsx
@@ -13,32 +13,38 @@ function LectureRecorder() {
       };
       mediaRecorder.start();
       setIsRecording(true);
+      alert("recording started")
     } catch (error) {
       console.error('Error capturing audio:', error);
     }
   };
 
   const stopRecording = async () => {
-    if (isRecording) {
-      const mediaRecorder = mediaRecorderRef.current;
-      mediaRecorder.stop();
+    console.log("click")
+    const mediaRecorder = mediaRecorderRef.current;
+    mediaRecorder.stop();
+    alert("recording stopped")
       setIsRecording(false);
+    // if (isRecording) {
+    //   const mediaRecorder = mediaRecorderRef.current;
+    //   mediaRecorder.stop();
+    //   setIsRecording(false);
 
       
-      const formData = new FormData();
-      formData.append('audioBlob', new Blob(recordedBlobs, { type: 'audio/webm' })); // Adjust format as needed
-      const response = await fetch('/api/save-recording', {
-        method: 'POST',
-        body: formData,
-      });
+    //   const formData = new FormData();
+    //   formData.append('audioBlob', new Blob(recordedBlobs, { type: 'audio/webm' })); 
+    //   const response = await fetch('/api/save-recording', {
+    //     method: 'POST',
+    //     body: formData,
+    //   });
 
-      if (response.ok) {
-        console.log('Recording saved successfully!');
-        setRecordedBlobs([]); 
-      } else {
-        console.error('Error saving recording:', response.statusText);
-      }
-    }
+    //   if (response.ok) {
+    //     console.log('Recording saved successfully!');
+    //     setRecordedBlobs([]); 
+    //   } else {
+    //     console.error('Error saving recording:', response.statusText);
+    //   }
+    // }
   };
 
   const mediaRecorderRef = useRef(null);
@@ -55,8 +61,9 @@ function LectureRecorder() {
   return (
     <div className="grid mt-40 bg-[#18212F] h-40 max-w-4xl self-center ">
       <button className="text-red-600" onClick={startRecording} disabled={isRecording}>
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
+        Start Recording
       </button>
+      <button className="text-red-600" onClick={stopRecording}> Stop Recording</button>
     </div>
   );
 }
