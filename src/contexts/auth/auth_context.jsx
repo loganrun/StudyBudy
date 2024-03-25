66c406254aca32f1e0fb02b7928a09ffc6ce5e15
loganrun <bigdog_41@hotmai.com>
2024-03-25 13:32:57 -0700

    cookies added

---
+++ src/contexts/auth/auth_context.jsx
@@ -0,0 +1,99 @@
+import { createContext, useContext, useMemo } from "react"
+import {useCookies} from 'react-cookies'
+import axios from "axios"
+
+const AppContext = createContext()
+
+
+
+export function UserProvider({children}) {
+    const [cookies, setCookies, removeCookies] = useCookies(['token'])
+    
+    const [userId, setUserId] = useState(cookies.userId)
+    const [userName, setUserName] = useState(cookies.userName)
+    const [userEmail, setUserEmail] = useState(cookies.userEmail)
+    const [userPhone, setUserPhone] = useState(cookies.userPhone)
+    const [userAddress, setUserAddress] = useState(cookies.userAddress)
+    const [userCity, setUserCity] = useState(cookies.userCity)
+    const [userState, setUserState] = useState(cookies.userState)
+    const [userZip, setUserZip] = useState(cookies.userZip)
+    const [userCountry, setUserCountry] = useState(cookies.userCountry)
+    const [userImage, setUserImage] = useState(cookies.userImage)
+
+    const login = async(formData)=>{
+
+        try {
+            let response = await axios({
+                method: "post",
+                url: "http://localhost:3000/api/auth",
+                data: formData,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+    
+            setCookies('token', response.data.token)
+    
+        } catch (error) {
+            
+        }
+    
+        
+    
+    
+    }
+    
+    const signUp = async(formData)=>{
+    
+        try {
+            let response = await axios({
+                method: "post",
+                url: "http://localhost:3000/api/users",
+                data: formData,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+    
+            setCookies('token', response.data.token)
+    
+        } catch (error) {
+            
+            
+        }
+    
+    }
+
+    const logout = () => {
+        ['token'].forEach((obj)=>{removeCookies(obj);});
+        setToken(null)
+        setUserId(null)
+        setUserName(null)
+        setUserEmail(null)
+        setUserPhone(null)
+        setUserAddress(null)
+        setUserCity(null)
+        setUserState(null)
+        setUserZip(null)
+        setUserCountry(null)
+        setUserImage(null)
+    }
+
+    const value = useMemo(
+        () => ({
+            cookies,
+            login,
+            signUp,
+            logout
+        }),
+        [cookies])
+  return (
+    <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useAuth = () =>{
+    return useContext(AppContext)
+}
