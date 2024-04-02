0651f0d93a03938682a30ab8dd5edee619c6765a
loganrun <bigdog_41@hotmai.com>
2024-04-02 09:01:15 -0700

    bucket added

--- backend/routes/api/audio.js
+++ backend/routes/api/audio.js
@@ -5,52 +5,54 @@ const path = require('path');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegStatic = require('ffmpeg-static');
 const Lectures = require('../../models/Lectures')
+const { Storage } = require('@google-cloud/storage');
 const fs = require('fs');
 const { Readable } = require('stream');
 const wav = require('wav');
 
-const convertBlobToWav = (blob) => {
-  return new Promise((resolve, reject) => {
-    const reader = new Readable();
-    reader.push(blob);
-    reader.push(null);
 
-    const wavStream = new wav.Writer({
-      channels: 1,
-      sampleRate: 44100,
-      bitDepth: 16,
-    });
+const upload = multer({ storage: multer.memoryStorage() });
 
-    reader.pipe(wavStream);
-
-    const chunks = [];
-
-    wavStream.on('data', (chunk) => {
-      chunks.push(chunk);
-    });
-
-    wavStream.on('end', () => {
-      const wavBuffer = Buffer.concat(chunks);
-      resolve(wavBuffer);
-    });
-
-    wavStream.on('error', (err) => {
-      reject(err);
-    });
-  });
-};
-
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); 
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`); 
-  },
+const storage = new Storage({
+  projectId: 'weetime',
+  keyFilename: '../../weetime-4763db2adbbf.json'
 });
-const upload = multer({ storage:storage });
 
+const bucketName = 'studybudy';
+
+pp.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
+    }
+
+    const fileExtension = path.extname(req.file.originalname);
+    const fileName = `${Date.now()}${fileExtension}`;
+    const file = storage.bucket(bucketName).file(fileName);
+
+    const stream = file.createWriteStream({
+      metadata: {
+        contentType: req.file.mimetype,
+      },
+    });
+
+    stream.on('error', (error) => {
+      console.error('Error uploading file:', error);
+      res.status(500).send('Error uploading file');
+    });
+
+    stream.on('finish', async () => {
+      await file.makePublic();
+      const fileUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`;
+      res.send(`File uploaded successfully: ${fileUrl}`);
+    });
+
+    stream.end(req.file.buffer);
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    res.status(500).send('Error uploading file');
+  }
+});
 
 router.get('/upload',async (req, res) => {
   try {
@@ -73,7 +75,7 @@ router.post("/upload",upload.single('file'), async(req,res) =>{
       transcript: req.body.transcript
     })
     await lecture.save()
-    res.json('saved')
+    res.json(req.file)
   } catch (error) {
     console.error(error.message)
     res.status(500).send('send error')
